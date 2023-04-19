e99507d461f8068c8cec71ef5b42679fa9b65180
goku <gokuhwaran@gmail.com>
2023-04-20 00:43:10 +0800

    new chunni

--- 17m760i/resource.lua
+++ 17m760i/resource.lua
@@ -1,11 +1,11 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-
+ 
 files {
+    'handling.meta',
     'vehicles.meta',
     'carvariations.meta',
     'carcols.meta',
-    'handling.meta',
-    'vehiclelayouts.meta'
+    'vehiclelayouts.meta',
 }
 
 data_file 'HANDLING_FILE' 'handling.meta'
